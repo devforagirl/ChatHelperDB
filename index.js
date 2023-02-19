e3eea6ecbb40a526d4640fa24a4e20dbc131eaf7
devforagirl <48156801+devforagirl@users.noreply.github.com>
2023-02-19 13:45:26 +0000

    optimize index

--- index.js
+++ index.js
@@ -8,15 +8,25 @@ require('dotenv').config();
 const app = express();
 const port = process.env.PORT;
 
+// Middleware
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-mongoose
-  .connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('Connected to MongoDB'))
-  .catch((err) => console.log('Error connecting to MongoDB:', err));
+// Connect to MongoDB
+mongoose.connect(process.env.DATABASE_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then(() => {
+  console.log('Connected to MongoDB');
+}).catch((error) => {
+  console.log('Error connecting to MongoDB:', error);
+});
 
+// Routes
 app.use('/api', apiRoutes);
 
-app.listen(port, () => console.log(`Server is running on port ${port}`));
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
